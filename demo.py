5acfc42c91446c6ca572a6b16d28b1a1a8bb18d3
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-10-28 09:23:27 +0100

    demo format

--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
 from splink import DuckDBLinker, levenshtein_level
 
+# lazy_level is of type LazyComparisonLevelFactory
+lazy_level = levenshtein_level("name", 2)
 
 settings = {"comparison_level": levenshtein_level("name", 2)}
 
-# levenshtein_level("name", 2) is of type LazyComparisonLevelFactory
 
 linker = DuckDBLinker(settings)
+linker.comparison_level.level_dict
+
+# Note greedy level
+greedy_level = levenshtein_level("name", 2, dialect="duckdb")
+greedy_level.level_dict
