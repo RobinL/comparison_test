4b462ab8b6bb54830416c7007a04d68450e2d5ee
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-10-28 15:18:31 +0100

    demonstrate composition of levels and input columns work

--- demo.py
+++ demo.py
@@ -1,11 +1,25 @@
-from splink import DuckDBLinker, levenshtein_level
+from splink import DuckDBLinker, AthenaLinker, levenshtein_level, jaro_winkler_level
+
 
 #########################
 # Suggested Usage Pattern (the settings object here does not have its full complexity)
 #########################
-settings = {"comparison_level": levenshtein_level("name", 2)}
+settings = {"comparison_level": levenshtein_level("name with space", 2)}
 linker = DuckDBLinker(settings)
+linker.comparison_level.level_dict
 
+settings = {"comparison_level": levenshtein_level("name with space", 2)}
+linker = AthenaLinker(settings)
+linker.comparison_level.level_dict
+
+
+settings = {"comparison_level": jaro_winkler_level("name with space", 2)}
+linker = DuckDBLinker(settings)
+linker.comparison_level.level_dict
+
+settings = {"comparison_level": jaro_winkler_level("name with space", 2)}
+linker = AthenaLinker(settings)
+linker.comparison_level.level_dict
 
 #########################
 # More detailed demo of how the levenshtein_level works:
@@ -13,9 +27,11 @@ linker = DuckDBLinker(settings)
 
 # To get a ComparisonLevel, call the levenshtein_level with a dialect
 cl = levenshtein_level("name", 2, dialect="duckdb")
+cl.level_dict
 
 # Get a lazy level, omit the dialect.  This returns a LazyComparisonLevelFactory
 lazy_level = levenshtein_level("name", 2)
+lazy_level
 
 # If you ask for a ComparisonLevel method on a LazyComparisonLevelFactory
 # you get a helpful warning
@@ -23,6 +39,7 @@ lazy_level.level_dict
 
 # You can manually activate a LazyComparisonLevelFactory to get a ComparisonLevel
 cl = lazy_level.get_dialected_level("duckdb")
+cl.level_dict
 
 
 # Highly simplified usage
